4d1d9a2a2a1c2be42319367024c15b50c05a3cf3
C-INFORM\metelitsa <b.metelitsa@center-inform.ru>
2023-05-24 16:01:43 +0300

    Initial commit

---
+++ DigitalDesignAbstractInstanceApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace DigitalDesignAbstractInstanceApp
+{
+    abstract class SomeAbstractClass
+    {
+        int IntegerValue;
+
+        public SomeAbstractClass()
+        {
+            
+        }
+        public string SomeProperty { get; set; }
+        public abstract void Method1();
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var obj = (SomeAbstractClass)typeof(RuntimeTypeHandle).GetMethod("Allocate", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[] { typeof(SomeAbstractClass) });
+            typeof(SomeAbstractClass).GetConstructor(Type.EmptyTypes).Invoke(obj, new object[0]);
+            obj.SomeProperty = "Wowie";
+            Console.WriteLine(obj.SomeProperty);
+            Console.ReadLine();
+        }
+    }
+}
